974f4b1566cfc8b93aa5277adc8f59cd04edd0cc
Jheil7 <grcsoccer7.jh@gmail.com>
2025-12-15 19:55:28 -0500

    Baseline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
 
+const mockURLs = [
+  "https://odusqhg2sm.ufs.sh/f/qBM8yc5L2V3jt89xBJoTLOeyMKUSzkjgi2P4ERZ8Hw1nuovr",
+  "https://odusqhg2sm.ufs.sh/f/qBM8yc5L2V3jLV3JFCrpet3HcR8Jm6rEdhxUN9A7uwKoPOSX",
+  "https://odusqhg2sm.ufs.sh/f/qBM8yc5L2V3jVq35gctXeFgiSR1n20uGzPmEjst9d7UTpWqZ",
+  "https://odusqhg2sm.ufs.sh/f/qBM8yc5L2V3jVykC8ntXeFgiSR1n20uGzPmEjst9d7UTpWqZ",
+];
+
+const mockImages = mockURLs.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        HelloWorld
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
       </div>
     </main>
   );
